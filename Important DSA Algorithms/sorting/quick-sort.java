ac5404dd64235ce211a31ff896d9f2f3c3d28af2
Ritvik <codebyritvik@gmail.com>
2022-01-28 23:05:25 +0530

    sort

---
+++ Important DSA Algorithms/sorting/quick-sort.java
@@ -0,0 +1,34 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int [] arr = {5, 4, 3, 2, 1};
+		quickSort(arr, 0, arr.length - 1);
+		for(int e : arr){
+		    System.out.println(e);
+		}
+	}
+	public static void quickSort(int [] arr, int low, int high){
+	    if(low >= high) return;
+	    int s = low;
+	    int e = high;
+	    int m = s + (e - s)/2;
+	    int pivot = arr[m];
+	    while(s <= e){
+	        while(arr[s] < pivot){
+	            s++;
+	        }
+	        while(arr[e] > pivot){
+	            e--;
+	        }
+	        if(s <= e){
+	            int temp = arr[s];
+	            arr[s] = arr[e];
+	            arr[e] = temp;
+	            s++;
+	            e--;
+	        }
+	    }
+	    quickSort(arr, low, e);
+	    quickSort(arr, s, high);
+	}
+}

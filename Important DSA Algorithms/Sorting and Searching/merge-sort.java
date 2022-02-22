9fbf84356bbc836c308264b602b67bfce84b313d
Ritvik <codebyritvik@gmail.com>
2022-02-22 10:24:51 +0530

    22-02-22

--- Important DSA Algorithms/Sorting and Searching/merge-sort.java
+++ Important DSA Algorithms/Sorting and Searching/merge-sort.java
@@ -96,3 +96,35 @@ public class Main
 	    }
 	}
 }
+
+//Simplified version (using shortcuts like arr[i] = 4, i++ as arr[i++] = 4)
+public class Main
+{
+	public static void main(String[] args) {
+	    int [] arr = {7, 2, 5, 4, 1, 9};
+	    mergeSort(arr, 0, arr.length - 1);
+		for(int i : arr) System.out.print(i);
+	}
+    public static void mergeSort(int [] arr, int s, int e){
+        if(s == e) return;
+	    int m = (s + e)/2;
+	    mergeSort(arr, s, m);
+	    mergeSort(arr, m + 1, e);
+        merge(arr, s, e);
+    }
+	public static void merge(int [] arr, int s, int e){
+	    int m = (s + e)/2;
+	    int i = s, n = m + 1, k = 0;
+	    int [] nums = new int[e - s + 1];
+	    while(s <= m && n <= e){
+	        if(arr[s] < arr[n]){
+	            nums[k++] = arr[s++];
+	        }else {
+	            nums[k++] = arr[n++];
+	        }
+	    }
+	    while(s <= m) nums[k++] = arr[s++];
+	    while(n <= e) nums[k++] = arr[n++];
+	    for(int p : nums) arr[i++] = p;
+	}
+}
